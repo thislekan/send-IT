3dc06182ba1f95d421c0474b5aa873c17f5badab
thislekan <lekanomoniyi@yahoo.com>
2018-11-04 00:36:48 +0100

    resolving redirecting issues on signup button click

--- UI/scripts/index.js
+++ UI/scripts/index.js
@@ -1,5 +1,5 @@
 const signupButton = document.getElementById('signup-btn');
 
 signupButton.addEventListener('click', () => {
-    location.href = '../UI/views/signup.html'
+    location.href = '/UI/views/signup.html'
 });
