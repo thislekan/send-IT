af08157741057dbb5f718f472144769b8878ba5a
thislekan <lekanomoniyi@yahoo.com>
2018-11-07 22:37:38 +0100

    included routes in server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import routes from './routes/routes';
 
 const server = express();
 server.use(bodyParser.json());
 const port = process.env.PORT || 3000;
+server.use(routes);
 server.listen(port, () => console.log(`server is listening on port ${port}`));
+
+// export { server }
