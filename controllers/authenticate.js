8b53d8680799a83b82712d3a4787a34adc86865d
thislekan <lekanomoniyi@yahoo.com>
2018-11-08 10:41:49 +0100

    Dummy controller

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -1,8 +1,12 @@
-import store from '../db/store'
+/* eslint-disable no-console */
+import store from '../db/store';
+
 
 const authenticate = (req, res, next) => {
-    res.send('Route Found');
-    // console.log(store);
-}
+  res.send('Route Found');
+  console.log(store);
+  next();
+};
 
-export { authenticate as default }
+
+export { authenticate as default };
