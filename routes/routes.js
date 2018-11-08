e049d6b8c124851c1e29ff77cbc00b1a83ed5da2
thislekan <lekanomoniyi@yahoo.com>
2018-11-08 10:43:33 +0100

    updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,30 @@
+/* eslint-disable no-console */
 import express from 'express';
-import authenticate from '../controllers/authenticate';
+// import authenticate from '../controllers/authenticate';
+import userHandler from '../controllers/userHandler';
+import parcelHandler from '../controllers/parcelHandler';
 
 const app = express.Router();
 
-app.route('/parcel')
-    .get(authenticate);
+// app.route('/parcel')
+//   .get(authenticate);
 
-// app.get('/parcel', authenticate);
+app.route('/create-user')
+  .post(userHandler.confirmUser, userHandler.createUser);
 
-export { app as default }
+app.route('/login')
+  .post(userHandler.logUserIn);
+
+app.route('/parcels')
+  .post(parcelHandler.createParcel);
+
+app.route('/parcels')
+  .get(parcelHandler.getAllParcels);
+
+app.route('/parcels/:id')
+  .get(parcelHandler.getParcelsById);
+
+app.route('/users/:userid/parcels')
+  .get(parcelHandler.getUserParcels);
+
+export { app as default };
