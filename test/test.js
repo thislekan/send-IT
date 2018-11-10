358280e488a728d07077d0767e10f1f4ea553f2e
thislekan <lekanomoniyi@yahoo.com>
2018-11-10 23:51:30 +0100

    Completed tests

--- test/test.js
+++ test/test.js
@@ -148,7 +148,6 @@ describe('Create parcel order with non existent user', () => {
 describe('Get all parcel', () => {
   it('Should return all parcels', (done) => {
     request.get(`${apiVersion}parcels/`)
-      // .set('userid', 'opojui')
       .end((error, response) => {
         expect(response.status).to.equal(200);
         expect(typeof (response.body)).to.equal('object');
@@ -164,6 +163,86 @@ describe('Get parcels by Id', () => {
         expect(response.status).to.equal(200);
         expect(typeof (response.body)).to.equal('object');
         expect(response.body.id).to.equal(parcelId);
+        done();
+      });
+  });
+});
+
+describe('Get parcels with non existent Id', () => {
+  it('Should return an error', (done) => {
+    request.get(`${apiVersion}parcels/ABL8-U`)
+      .end((error, response) => {
+        expect(response.status).to.equal(404);
+        expect(typeof (response.body.error)).to.equal('string');
+        expect(response.body.error).to.equal('Parcel with ID: ABL8-U not found');
+        done();
+      });
+  });
+});
+
+describe('Get user parcels', () => {
+  it('Should return all parcels created by user', (done) => {
+    request.get(`${apiVersion}users/${userid}/parcels`)
+      .end((error, response) => {
+        expect(response.status).to.equal(200);
+        expect(typeof (response.body)).to.equal('object');
+        expect(response.body[0].id).to.equal(parcelId);
+        done();
+      });
+  });
+});
+
+describe('Get user parcels with non existing Id', () => {
+  it('Should return all parcels created by user', (done) => {
+    request.get(`${apiVersion}users/ab0Ihn/parcels`)
+      .end((error, response) => {
+        expect(response.status).to.equal(404);
+        expect(typeof (response.body)).to.equal('object');
+        expect(response.body.error)
+          .to.equal('No parcel record for the user exist');
+        done();
+      });
+  });
+});
+
+describe('Cancel user parcels with wrong parcelId', () => {
+  it('Should return an error', (done) => {
+    request.put(`${apiVersion}parcels/${userid}/cancel`)
+      .set('userid', userid)
+      .end((error, response) => {
+        expect(response.status).to.equal(404);
+        expect(typeof (response.body)).to.equal('object');
+        expect(response.body.error)
+          .to.equal(`No parcel with ID: ${userid} was found`);
+        done();
+      });
+  });
+});
+
+describe('Cancel user parcels with wrong userId', () => {
+  it('Should return an error', (done) => {
+    request.put(`${apiVersion}parcels/${parcelId}/cancel`)
+      .set('userid', 'userid')
+      .end((error, response) => {
+        expect(response.status).to.equal(401);
+        expect(typeof (response.body)).to.equal('object');
+        expect(response.body.error)
+          .to.equal('You are not authorized to change this parcel');
+        done();
+      });
+  });
+});
+
+describe('Cancel user parcels', () => {
+  it('Should return specific canceled parcel', (done) => {
+    request.put(`${apiVersion}parcels/${parcelId}/cancel`)
+      .set('userid', userid)
+      .end((error, response) => {
+        expect(response.status).to.equal(200);
+        expect(typeof (response.body)).to.equal('object');
+        expect(response.body.message)
+          .to.equal('Parcel succefully canceled');
+        expect(response.body.parcel.id).to.equal(parcelId);
         done();
       });
   });
