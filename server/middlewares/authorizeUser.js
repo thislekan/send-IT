c79dea75c61b63762977a7668d4dda2b91536652
thislekan <lekanomoniyi@yahoo.com>
2018-11-13 18:59:33 +0100

    remove console rule

--- server/middlewares/authorizeUser.js
+++ server/middlewares/authorizeUser.js
@@ -1,17 +1,15 @@
-/* eslint-disable no-console */
 import store from '../db/store';
 
 const confirmUser = (req, res, next) => {
   const query = req.body;
   if (!query.email || !query.password) {
-    res.status(400).send({ error: 'Email and password are required.' });
+    return res.status(400).send({ error: 'Email and password are required.' });
   }
   const alreadyExist = store.users.findIndex(element => element.email === query.email);
   if (alreadyExist !== -1) {
-    res.status(403).send({ error: 'The user already exist.' });
-  } else {
-    next();
+    return res.status(403).send({ error: 'The user already exist.' });
   }
+  return next();
 };
 
 export default confirmUser;
