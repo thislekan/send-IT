32093a41553381946471092582ccfc665a44c133
thislekan <lekanomoniyi@yahoo.com>
2018-11-13 18:33:21 +0100

    Update coverage script
    - Ignore nyc_output
    - Add return to userhandler responses
    - Update test and coverage scripts

--- server/controllers/userHandler.js
+++ server/controllers/userHandler.js
@@ -18,38 +18,36 @@ export default {
     const newUser = req.body;
 
     if (!validator.isEmail(newUser.email)) {
-      res.status(400).send({ error: 'Email invalid. Please signup with a valid email' });
+      return res.status(400).send({ error: 'Email invalid. Please signup with a valid email' });
     }
 
     const userID = idGenerator();
     const userDetails = new User(newUser.email, newUser.password, userID);
 
     if (newUser.password.length < 6 || typeof (newUser.password) !== 'string') {
-      res.status(400).send({ error: 'The password needs to be 6 or more characters' });
-    } else {
-      store.users.push(userDetails);
-      res.status(201).send({ email: newUser.email, id: userID });
+      return res.status(400).send({ error: 'The password needs to be 6 or more characters' });
     }
+
+    store.users.push(userDetails);
+    return res.status(201).send({ email: newUser.email, id: userID });
   },
   logUserIn: (req, res) => {
     const query = req.body;
 
     if (!query.email || !query.password) {
-      res.status(400).send({ error: 'Password and Email required to log in.' });
+      return res.status(400).send({ error: 'Password and Email required to log in.' });
     }
 
     if (store.users.length > 0) {
       const foundUser = store.users.find(element => element.email === query.email);
       if (!foundUser) {
-        res.status(400).send({ error: 'The email or password is incorrect' });
+        return res.status(400).send({ error: 'The email or password is incorrect' });
       }
       if (query.password !== foundUser.password) {
-        res.status(400).send({ error: 'The email or password is incorrect' });
-      } else {
-        res.status(200).send(foundUser);
+        return res.status(400).send({ error: 'The email or password is incorrect' });
       }
-    } else {
-      res.status(404).send({ error: 'Login credentials not found. please sign up' });
+      return res.status(200).send(foundUser);
     }
+    return res.status(404).send({ error: 'Login credentials not found. please sign up' });
   },
 };
