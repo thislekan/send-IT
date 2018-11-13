ac64c8b76faedc9575ef7672893a712cc02ffdb4
thislekan <lekanomoniyi@yahoo.com>
2018-11-13 18:46:26 +0100

    Refactor parcelHandler

--- server/controllers/parcelHandler.js
+++ server/controllers/parcelHandler.js
@@ -21,84 +21,76 @@ export default {
     if (store.users.length > 0) {
       const foundUser = store.users.find(element => element.id === userid);
       if (!foundUser) {
-        res.status(400).send({ error: 'User does not exist.' });
-      } else {
-        foundUser.orders.unshift(newOrder);
-        store.parcels.unshift(newOrder);
-
-        res.status(201).send(newOrder);
+        return res.status(400).send({ error: 'User does not exist.' });
       }
-    } else {
-      res.status(401).send({
-        error: 'User not authorized to create parcel. Please sign up.',
-      });
+      foundUser.orders.unshift(newOrder);
+      store.parcels.unshift(newOrder);
+
+      return res.status(201).send(newOrder);
     }
+    return res.status(401).send({
+      error: 'User not authorized to create parcel. Please sign up.',
+    });
   },
   getAllParcels: (req, res) => {
     if (store.parcels.length < 1) {
-      res.status(404).send({ error: 'No parcel record found.' });
-    } else {
-      const allParcels = store.parcels;
-      res.status(200).send(allParcels);
+      return res.status(404).send({ error: 'No parcel record found.' });
     }
+    const allParcels = store.parcels;
+    return res.status(200).send(allParcels);
   },
   getParcelsById: (req, res) => {
     const query = req.params.parcelId;
 
     if (!store.parcels.length) {
-      res.status(404).send({ error: 'No parcel record exist in Database' });
-    } else {
-      const foundParcel = store.parcels.find(element => element.id === query);
-
-      if (!foundParcel) {
-        res.status(404).send({ error: `Parcel with ID: ${query} not found` });
-      } else {
-        res.status(200).send(foundParcel);
-      }
+      return res.status(404).send({ error: 'No parcel record exist in Database' });
     }
+    const foundParcel = store.parcels.find(element => element.id === query);
+    if (!foundParcel) {
+      return res.status(404).send({ error: `Parcel with ID: ${query} not found` });
+    }
+    return res.status(200).send(foundParcel);
   },
   getUserParcels: (req, res) => {
     const query = req.params.userId;
 
     if (store.users.length < 1) {
-      res.status(404).send({ error: 'No parcel record exist on the Database yet.' });
-    } else {
-      const foundUser = store.users.find(element => element.id === query);
-
-      if (!foundUser) {
-        res.status(404).send({ error: 'No parcel record for the user exist' });
-      } else {
-        res.status(200).send(foundUser.orders);
-      }
+      return res.status(404).send({ error: 'No parcel record exist on the Database yet.' });
     }
+
+    const foundUser = store.users.find(element => element.id === query);
+    if (!foundUser) {
+      return res.status(404).send({ error: 'No parcel record for the user exist' });
+    }
+
+    return res.status(200).send(foundUser.orders);
   },
   cancelParcel: (req, res) => {
     const { parcelId } = req.params;
     const { userid } = req.headers;
 
     if (!store.parcels.length) {
-      res.status(404).send({ error: 'No parcel record exist on the Database yet.' });
-    } else {
-      const foundParcel = store.parcels.find(element => element.id === parcelId);
-      const foundUser = store.users.find(element => element.id === userid);
-
-      if (!foundParcel) {
-        res.status(404).send({ error: `No parcel with ID: ${parcelId} was found` });
-      }
-
-      if (foundParcel.user !== userid) {
-        res.status(401).send({ error: 'You are not authorized to change this parcel' });
-      } else {
-        const indexOfParcelForUser = foundUser.orders.indexOf(foundParcel);
-        const indexOfParcelForParcels = store.parcels.indexOf(foundParcel);
-
-        const canceledParcel = foundUser.orders[indexOfParcelForUser];
-
-        foundUser.orders.splice(indexOfParcelForUser, 1);
-        store.parcels.splice(indexOfParcelForParcels, 1);
-
-        res.send({ message: 'Parcel succefully canceled', parcel: canceledParcel });
-      }
+      return res.status(404).send({ error: 'No parcel record exist on the Database yet.' });
     }
+
+    const foundParcel = store.parcels.find(element => element.id === parcelId);
+    const foundUser = store.users.find(element => element.id === userid);
+
+    if (!foundParcel) {
+      return res.status(404).send({ error: `No parcel with ID: ${parcelId} was found` });
+    }
+
+    if (foundParcel.user !== userid) {
+      return res.status(401).send({ error: 'You are not authorized to change this parcel' });
+    }
+    const indexOfParcelForUser = foundUser.orders.indexOf(foundParcel);
+    const indexOfParcelForParcels = store.parcels.indexOf(foundParcel);
+
+    const canceledParcel = foundUser.orders[indexOfParcelForUser];
+
+    foundUser.orders.splice(indexOfParcelForUser, 1);
+    store.parcels.splice(indexOfParcelForParcels, 1);
+
+    return res.send({ message: 'Parcel succefully canceled', parcel: canceledParcel });
   },
 };
